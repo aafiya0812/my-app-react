957e8f488039b13569f605371c35e78b9414a435
aafiya-ranium <aafiya@ranium.in>
2021-02-16 19:08:05 +0530

    ini commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { API_BASE_URL } from './config';
+import axios from 'axios';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +25,42 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+
+class CarColor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            brand: "Ford",
+            model: "Mustang",
+            color: "red",
+            year: 1964
+        };
+    }
+    changeColor = () => {
+        console.log(`${process.env.REACT_APP_API_BASE_URL}`);
+        axios.get(process.env.REACT_APP_API_BASE_URL+`/users`)
+        .then((response) => {
+            console.log(response);
+        })
+        // this.setState({color: "blue"});
+    }
+    render() {
+        return (
+            <div>
+                <h1>My {this.state.brand}</h1>
+                <p>
+                    It is a {this.state.color}
+                    {this.state.model}
+                    from {this.state.year}.
+                </p>
+                <button
+                type="button"
+                onClick={this.changeColor}
+                >Change color</button>
+            </div>
+        );
+    }
+}
+
+export default CarColor;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CarColor from './App.js';
+import Garage from './props-example.js';
 
-ReactDOM.render(
+ReactDOM.render(<CarColor />, document.getElementById('root'));
+
+ReactDOM.render(<Garage />, document.getElementById('root1'));
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

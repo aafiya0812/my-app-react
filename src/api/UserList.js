997197e514fbfc8dad39a582de75c1bd4feee0aa
aafiya-ranium <aafiya@ranium.in>
2021-02-18 13:02:09 +0530

    delete user feature created, list of user design changes.

--- src/api/UserList.js
+++ src/api/UserList.js
@@ -8,6 +8,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Container from '@material-ui/core/Container';
+import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 
 class UserList extends React.Component {
     constructor() {
@@ -29,6 +31,13 @@ class UserList extends React.Component {
         })
     }
 
+    deleteUser = (userId) => {
+        axios.delete(process.env.REACT_APP_API_BASE_URL+`/user/delete/${userId}`)
+        .then((response) => {
+            this.getUsers();
+        })
+    }
+
     render() {
         if (this.state.hasError) {
             return <h1>Something went wrong.</h1>;
@@ -36,28 +45,34 @@ class UserList extends React.Component {
 
         return (
             <div className="container">
-                <h1>List of Users:</h1>
-                <Grid container>
-                    <Grid item xs={12} md={6}>
-                        <div>
-                            { this.state.users.map(( user, index ) =>
-                                <List key={ index }>
-                                    <ListItem>
-                                        <ListItemText
-                                        primary={ user.name }
-                                        secondary='Secondary text'
-                                        />
-                                        <ListItemSecondaryAction>
-                                        <IconButton edge="end" aria-label="delete">
-                                            <DeleteIcon />
-                                        </IconButton>
-                                        </ListItemSecondaryAction>
-                                    </ListItem>
-                                </List>
-                            ) }
-                        </div>
+                <Container maxWidth="lg">
+                    <h1>List of Users:</h1>
+                    <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center">
+                        <Grid item xs={12} md={6}>
+                            <div>
+                                { this.state.users.map(( user, index ) =>
+                                    <List key={ index }>
+                                        <ListItem>
+                                            <ListItemText
+                                            primary={ user.name }
+                                            secondary='Secondary text'
+                                            />
+                                            <ListItemSecondaryAction>
+                                                <IconButton edge="end" aria-label="delete" onClick= {() => {this.deleteUser(user.id)}}>
+                                                    <DeleteRoundedIcon />
+                                                </IconButton>
+                                            </ListItemSecondaryAction>
+                                        </ListItem>
+                                    </List>
+                                ) }
+                            </div>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Container>
             </div>
         );
     }
